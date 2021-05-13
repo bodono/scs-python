924e034e267d3b3e6793f82e57ac3283fc429d70
H. Vetinari <h.vetinari@gmx.com>
2021-05-13 19:09:00 +0200

    basic parametrization of test_scs_rand.py

--- test/test_scs_rand.py
+++ test/test_scs_rand.py
@@ -12,6 +12,12 @@ def import_error(msg):
 
 
 try:
+  import pytest
+except ImportError:
+  import_error('Please install pytest to run tests.')
+  raise
+
+try:
   import scs
 except ImportError:
   import_error('You must install the scs module before running tests.')
@@ -29,10 +35,6 @@ try:
 except ImportError:
   import_error('Please install scipy.')
   raise
-
-
-def check_solution(solution, expected):
-  assert_almost_equal(solution, expected, decimal=2)
 
 
 def assert_(str1, str2):
@@ -70,30 +72,29 @@ K = {
 m = tools.get_scs_cone_dims(K)
 
 
-def test_feasible():
+@pytest.mark.parametrize("use_indirect", [False, True])
+def test_feasible(use_indirect):
   for i in range(num_feas):
     data, p_star = tools.gen_feasible(K, n=m // 3, density=0.1)
 
-    sol = scs.solve(data, K, use_indirect=False, **opts)
-    check_solution, np.dot(data['c'], sol['x']), p_star
-    check_solution, np.dot(-data['b'], sol['y']), p_star
-
-    sol = scs.solve(data, K, use_indirect=True, **opts)
-    check_solution, np.dot(data['c'], sol['x']), p_star
-    check_solution, np.dot(-data['b'], sol['y']), p_star
+    sol = scs.solve(data, K, use_indirect=use_indirect, **opts)
+    assert_almost_equal(np.dot(data['c'], sol['x']), p_star, decimal=2)
+    assert_almost_equal(np.dot(-data['b'], sol['y']), p_star, decimal=2)
 
 
-def test_infeasible():
+@pytest.mark.parametrize("use_indirect", [False, True])
+def test_infeasible(use_indirect):
   for i in range(num_infeas):
     data = tools.gen_infeasible(K, n=m // 3)
 
-    check_infeasible, scs.solve(data, K, use_indirect=False, **opts)
-    check_infeasible, scs.solve(data, K, use_indirect=True, **opts)
+    sol = scs.solve(data, K, use_indirect=use_indirect, **opts)
+    check_infeasible(sol)
 
 
-def test_unbounded():
+@pytest.mark.parametrize("use_indirect", [False, True])
+def test_unbounded(use_indirect):
   for i in range(num_unb):
     data = tools.gen_unbounded(K, n=m // 2)
 
-    check_unbounded, scs.solve(data, K, use_indirect=False, **opts)
-    check_unbounded, scs.solve(data, K, use_indirect=True, **opts)
+    sol = scs.solve(data, K, use_indirect=use_indirect, **opts)
+    check_unbounded(sol)
