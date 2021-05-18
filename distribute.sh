9489f55b342f0ec1fc8eef5b8de5c0c7352017be
Naphat Sanguansin <nsanguansin@vise.com>
2021-05-18 08:17:30 +0100

    Revert "update distribute script", this is not needed
    This reverts commit 26ffa555c2a2e188a65300a2917c73a37bfb1e85.

--- distribute.sh
+++ distribute.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # distributing wheels broken due to blas linking issues
-pip install build
-#python -m build --sdist --wheel
-python -m build --sdist
+#python setup.py sdist bdist_wheel
+python setup.py sdist
 twine upload dist/*
 
