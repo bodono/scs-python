6c55cf1618a9c994380b5ab7e23efbccf6cd9bea
Brendan O'Donoghue <bodonoghue85@gmail.com>
2022-02-21 18:46:26 +0000

    Improve warm-start parsing and error message (#60)
    * improve warm-start parsing and error message
    
    * add comment
    
    * fix shape bug

--- test/test_scs_object.py
+++ test/test_scs_object.py
@@ -75,3 +75,12 @@ def test_warm_start(use_indirect):
     sol = solver.solve()
     assert_almost_equal(sol["x"][0], 1.0, decimal=2)
     assert_array_less(sol["info"]["iter"], 10)
+
+    sol = solver.solve(x=None)
+    sol = solver.solve(x=np.array([7.0]), y=None, s=None)
+    sol = solver.solve(
+        x=np.array([7.0]), y=np.array([1.0, 2.0]), s=np.array([3.0, 4.0])
+    )
+
+    with pytest.raises(ValueError):
+        sol = solver.solve(x=np.array([1.0, 2.0]))
