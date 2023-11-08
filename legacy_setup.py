a5069385e3e0ef9573d0e58b976e16844e90255a
Brendan O'Donoghue <bodonoghue85@gmail.com>
2023-11-08 11:28:19 +0000

    bump to 3.2.4

--- legacy_setup.py
+++ legacy_setup.py
@@ -288,7 +288,7 @@ def install_scs(**kwargs):
 
     setup(
         name="scs",
-        version="3.2.3",
+        version="3.2.4",
         author="Brendan O'Donoghue",
         author_email="bodonoghue85@gmail.com",
         url="http://github.com/cvxgrp/scs",
