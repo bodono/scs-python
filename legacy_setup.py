89678ebdfe9435d40eb95b12ca8662f4d6404e8c
Brendan O'Donoghue <bodonoghue85@gmail.com>
2024-09-26 10:42:13 +0100

    change conda setuptools

--- legacy_setup.py
+++ legacy_setup.py
@@ -297,8 +297,8 @@ def install_scs(**kwargs):
         packages=["scs"],
         ext_modules=ext_modules,
         cmdclass={"build_ext": build_ext_scs},
-        setup_requires=["numpy >= 1.7", "setuptools < 60.0"],
-        install_requires=["numpy >= 1.7", "scipy >= 0.13.2", "setuptools < 60.0"],
+        setup_requires=["numpy >= 1.7"],
+        install_requires=["numpy >= 1.7", "scipy >= 0.13.2"],
         license="MIT",
         zip_safe=False,
         # TODO: update this:
