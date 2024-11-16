07ba527ea73336884663e4b7ac16a1b63f8db733
Enzo Busseti <enzo.busseti@me.com>
2024-11-16 18:58:47 +0400

    Revert "trying 3.7"
    This reverts commit c1ebc02730e1e94067848b7ae17d81df83c8ee04.

--- scs/scspy.c
+++ scs/scspy.c
@@ -4,8 +4,8 @@
 /*Standard macro for CPython API back-compatibility*/
 #define PY_SSIZE_T_CLEAN
 
-/*Use ABI3 limited API for CPython forward compatibility, 3.7+*/
-#define Py_LIMITED_API 0x03070000
+/*Use ABI3 limited API for CPython forward compatibility, 3.6+*/
+/* #define Py_LIMITED_API 0x03060000*/
 
 /* IMPORTANT: This code now uses numpy array types. It is a private C module
  * in the sense that end users only see the front-facing Python code in
