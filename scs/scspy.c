44daaedefcf31a1506f4374303feb1cbd77b430b
Enzo Busseti <enzo.busseti@me.com>
2024-11-16 18:48:05 +0400

    broke something with PyTypeObject

--- scs/scspy.c
+++ scs/scspy.c
@@ -5,7 +5,7 @@
 #define PY_SSIZE_T_CLEAN
 
 /*Use ABI3 limited API for CPython forward compatibility, 3.6+*/
-#define Py_LIMITED_API 0x03060000
+/* #define Py_LIMITED_API 0x03060000*/
 
 /* IMPORTANT: This code now uses numpy array types. It is a private C module
  * in the sense that end users only see the front-facing Python code in
